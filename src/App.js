e57c7946634a0054675c33e5d176cee113d49e0a
noor-khan-123 <nooremaan0312@gmail.com>
2024-08-25 19:31:15 +0500

    landing page

--- src/App.js
+++ src/App.js
@@ -1,23 +1,20 @@
-import logo from './logo.svg';
 import './App.css';
+import Home from './components/Home';
+import About from './components/About';
+import Work from './components/Work';
+import Testimonials from './components/Testimonials';
+import Contacts from './components/Contacts';
+import Footer from './components/Footers';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App">  
+      <Home/>
+      <About/>
+      <Work/>
+      <Testimonials/>
+      <Contacts/>
+      <Footer/>
     </div>
   );
 }
